778612b3a32f706de19daa9224a96e3257c8a7d4
Priyasekar01 <priyasekar0137@gmail.com>
2024-06-28 15:48:11 +0530

    first

---
+++ src/main/java/com/app/attendance/AttendanceManagementApplication.java
@@ -0,0 +1,24 @@
+package com.app.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@SpringBootApplication
+public class AttendanceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceManagementApplication.class, args);
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:message/message");
+		messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+		return messageSource;
+	}
+
+}
